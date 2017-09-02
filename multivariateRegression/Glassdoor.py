d10730da1944275f89353d5bc67428955c6ceb36
bmowry06 <bmowry@brennans-imac.home>
2017-09-02 09:39:23 -0500

    Added random folder for demonstration

--- multivariateRegression/Glassdoor.py
+++ multivariateRegression/Glassdoor.py
@@ -4,12 +4,46 @@ Created on Thu Aug 17 19:24:02 2017
 
 @author: bmowry
 """
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 import pandas as pd
+import re
 
+# import csv of glassdoor jobs data and label
 labels = ['Company', 'Location', 'Salary', 'Review', 'Recency']
 df = pd.read_csv('Glassdoor.csv', skiprows=1, names=labels)
 
-e1 = "$(\d+)k-"
-e2 = "-$(\d+)k"
-for i in range(len(df.Salary)):
-    df.Salary[i] = str(df.Salary[i])[12:]
+# convert to string so that nan float values can be filtered out
+df.Salary = df.Salary.astype(str)
+# filter out nan float values
+df = df[df.Salary != 'nan']
+# reindex
+df.index = range(len(df))
+
+# extract average of 25th and 75th quartiles. we use this to represent the 
+# salary
+for i in range(len(df.Salary[:])):
+    p = re.compile('\d+')
+    sal = p.findall(str(df.Salary[i]))
+    salMean = np.divide(int(sal[0]) + int(sal[1]), 2)
+    df.iloc[(i, 2)] = salMean
+#    df.Salary[i] = salMean
+  
+# number of bins for hist = sqrt of number of data points
+n_data = len(df.Salary)
+n_bins = np.sqrt(n_data)
+n_bins = int(n_bins)
+
+
+plt.title('Distribution of Data Science Salaries')
+plt.xlabel('Salary Range')
+plt.ylabel('Qty')
+plt.hist(df.Salary, bins=n_bins, alpha = 0.75, histtype='bar', ec='black')
+
+#sns.set()
+#sns.swarmplot(x='Location', y='Salary', data=df)
+#plt.xlabel('Location')
+#plt.ylabel('Salary')
+#plt.show()
+
